534e7ce8fbc8f83fedf072db58eee8fb7a40e3f7
petruamortoae <petru.amortoae@gmail.com>
2017-11-24 12:42:40 +0200

    First version

---
+++ Tema_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class SuneteDeAnimale
+{
+    public static void Main()
+    {
+        string[] listaAnimale = new string[5];
+
+        listaAnimale[0] = "Cainele face ham";
+        listaAnimale[1] = "Pisica face miau";
+        listaAnimale[2] = "Oaia face bee";
+        listaAnimale[3] = "Rata face mac-mac";
+        listaAnimale[4] = "Ursul face mor-mor";
+
+        Console.Write("\nAnimalele urmate de sunetele scoase de acestea sunt: ");
+        foreach (string animal in listaAnimale)
+        {
+            Console.Write("\n{0}  ", animal);
+        }
+        Console.ReadLine();
+    }
+}
